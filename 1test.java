2720db4a8eaf98b40a5e1a7e12d47cba7cfc9fd2
IvanM6767 <vanioivamami@gmail.com>
2024-06-23 23:41:45 +0300

    1

---
+++ 1test.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Введите число b: ");
+        int b = scanner.nextInt();
+
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        System.out.println("Сложение: " + (a + b));
+        System.out.println("Вычитание: " + (a - b));
+        
+        if (b != 0) {
+            System.out.println("Деление: " + ((double) a / b));
+        } else {
+            System.out.println("Деление на ноль невозможно");
+        }
+
+        System.out.println("Умножение: " + (a * b));
+    }
+}
---
+++ 3test.java
@@ -0,0 +1,22 @@
+// Имя файла: Main3.java
+import java.util.Scanner;
+
+public class Main { 
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите первую строку (a): ");
+        String a = scanner.nextLine();
+
+        System.out.print("Введите вторую строку (b): ");
+        String b = scanner.nextLine();
+
+        // Сравнение строк
+        if (a.equals(b)) {
+            System.out.println("Строки идентичны");
+        } else {
+            System.out.println("Строки неидентичны");
+        }
+    }
+}
